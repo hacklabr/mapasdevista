a2de19ffefc4fe032be2cfc4dd6fa20349eb7708
Fabio Montefuscolo <fabio.montefuscolo@gmail.com>
2011-07-12 09:44:44 -0300

    [fix] javascripts faltantes no último commit

---
+++ src/wp-content/themes/mapasdevista/admin/metabox.js
@@ -0,0 +1,118 @@
+jQuery(document).ready(function() {
+    var $ = jQuery;
+
+    var map_options = {
+        'zoom':14,
+        'scrollwheel':true,
+        'draggableCursor':'default',
+        'center': new google.maps.LatLng(-23.56367, -46.65372),
+        'mapTypeId': google.maps.MapTypeId.ROADMAP
+        }
+    googlemap = new google.maps.Map(document.getElementById("mpv_canvas"), map_options);
+    var googlemarker = null;
+
+    function fill_fields(lat, lng) {
+        $("#mpv_lat").val(lat);
+        $("#mpv_lon").val(lng);
+    }
+
+    // place a marker on the map
+    function load_post_marker(lat, lng) {
+        try{
+            lat = parseFloat(lat);
+            lng = parseFloat(lng);
+            if(lat && lng) {
+                if(googlemarker) {
+                    googlemarker.setPosition(new google.maps.LatLng(lat, lng));
+                }else{
+                    fill_fields(lat, lng);
+                    googlemarker = new google.maps.Marker({
+                        map: googlemap,
+                        draggable: true,
+                        position: new google.maps.LatLng(lat, lng)
+                    });
+                    googlemap.panTo(googlemarker.getPosition());
+                }
+            }
+            return googlemarker;
+        } catch(e) {  }
+        return null;
+    }
+
+    // plot marker on saved location and define map drag event
+    if(load_post_marker($("#mpv_lat").val(), $("#mpv_lon").val())) {
+        google.maps.event.addListener(googlemarker, 'drag', function(e) {fill_fields(e.latLng.lat(), e.latLng.lng());});
+    }
+
+    // define map click event
+    var clicklistener = google.maps.event.addListener(googlemap, 'click', function(event) {
+        place_marker(event.latLng);
+    });
+
+    // callback for map click event
+    var place_marker = function(location) {
+        if(googlemarker === null) {
+            load_post_marker(location.lat(), location.lng());
+        } else {
+            googlemarker.setPosition(location);
+            fill_fields(location.lat(), location.lng());
+            google.maps.event.addListener(googlemarker, 'drag', function(e) {fill_fields(e.latLng.lat(), e.latLng.lng());});
+        }
+        if(clicklistener){
+            google.maps.event.removeListener(clicklistener);
+        }
+        google.maps.event.addListener(googlemap, 'click', function(e) {
+            googlemarker.setPosition(e.latLng);
+            fill_fields(e.latLng.lat(), e.latLng.lng());
+        });
+    }
+
+    // activate google service
+    var geocoder = new google.maps.Geocoder();
+
+    // callback to handle google geolocation result
+    function geocode_callback(results, status) {
+        if (status == google.maps.GeocoderStatus.OK) {
+            var location = results[0].geometry.location;
+            googlemap.setCenter(location);
+            fill_fields(location.lat(), location.lng());
+
+            if(googlemarker) {
+                googlemarker.setPosition(location)
+            } else {
+                googlemarker = new google.maps.Marker({
+                    map: googlemap,
+                    draggable: true,
+                    position: location
+                });
+            }
+        }
+    }
+
+    // the search bar, where user can type an address
+    $("#mpv_search_address").keypress(function(e){
+        if(e.charCode===13){ // carriage return
+            geocoder.geocode({'address': $(this).val()}, geocode_callback);
+            return false;
+        }
+    });
+
+    // the button to place marker on specified coords
+    $("#mpv_load_coords").click(function(){load_post_marker($("#mpv_lat").val(), $("#mpv_lon").val())});
+
+    // change the map pin
+    $("#pinlist input").change(function(e) {
+        var id = $(this).attr('id');
+        var anchor = pinsanchor[id];
+        var img_el = $(this).parents('div.icon').find('img');
+
+        var pin = new google.maps.MarkerImage(img_el.attr('src'),
+                    new google.maps.Size(img_el.width(), img_el.height()),
+                    new google.maps.Point(0, 0),
+                    new google.maps.Point(anchor.x, anchor.y));
+        googlemarker.setIcon(pin);
+    }).change();
+
+    // let the user resize map
+    $("#mpv_canvas").resizable({ handles: 's'});
+});
---
+++ src/wp-content/themes/mapasdevista/admin/pins.js
@@ -0,0 +1,92 @@
+jQuery(document).ready(function() {
+    var $ = jQuery;
+    /* the user will change coords values clicking on image or using
+     * keyboard arrows. both image and <input /> delegate focus to
+     * the <input />*/
+
+    // retrieve image dimensions
+    var image_width = parseInt($("#the-image").attr('width'));
+    var image_height = parseInt($("#the-image").attr('height'));
+
+    // retrive image wrapper element and fix its dimensions
+    var image_panel_el = document.getElementById("image-panel");
+    $(image_panel_el).css('width', image_width).css('height', image_height);
+
+    // represent the desired point that will be the anchor on map
+    var image_anchor = {
+        'x': 0,
+        'y': 0,
+
+        // set X position and draw it on screen
+        'set_x' : function (x) {
+            this.x = x < 0 ? 0 : x > image_width ? image_width : x ; // 0 < x < image_width
+            $("#image-x-ruler").css('left', image_panel_el.offsetLeft + this.x);
+        },
+
+        // set Y position and draw it on screen
+        'set_y' : function (y) {
+            this.y = y < 0 ? 0 : y > image_height ? image_height : y ; // 0 < x < image_width
+            $("#image-y-ruler").css('top', image_panel_el.offsetTop + this.y);
+        }
+    }
+
+    // draw stored values when page LOAD
+    $(window).load(function() {
+        var initial = $("#pin_anchor").val().match(/^([0-9]+),([0-9]+)$/);
+        if( initial ) {
+            image_anchor.set_x(parseInt(initial[1]));
+            image_anchor.set_y(parseInt(initial[2]));
+        } else {
+            image_anchor.set_x(Math.floor(image_width / 2));
+            image_anchor.set_y(Math.floor(image_height / 2));
+        }
+    });
+
+    // fix the image and rulers when user resizes the browser
+    $(window).resize(function() {
+        image_anchor.set_x(image_anchor.x);
+        image_anchor.set_y(image_anchor.y);
+    });
+
+    // keyboard event to move rulers
+    var accel = 0.4;
+    var veloc = 1;
+    $("#pin_anchor").keydown(function(e) {
+        if(e.keyCode == 37) {        // <
+            if(image_anchor.x > 0) image_anchor.set_x(Math.floor(image_anchor.x - veloc));
+        } else if(e.keyCode == 38) { // ^
+            if(image_anchor.y > 0) image_anchor.set_y(Math.floor(image_anchor.y - veloc));
+        } else if(e.keyCode == 39) { // >
+            if(image_anchor.x < image_width) image_anchor.set_x(Math.floor(image_anchor.x + veloc));
+        } else if(e.keyCode == 40) { // v
+            if(image_anchor.y < image_height) image_anchor.set_y(Math.floor(image_anchor.y + veloc));
+        }
+        if( e.keyCode > 36 && e.keyCode < 41 ){
+            $(this).val(image_anchor.x + "," + image_anchor.y);
+            veloc = veloc + accel;
+        }
+        return false;
+    });
+    // reset velocity
+    $("#pin_anchor").keyup(function(e) { veloc = 1; });
+
+    // mouse events to move rulers
+    var mousepressed = false;
+    $("#the-image").mousedown(function(e) {
+        mousepressed = true;
+        image_anchor.set_x(e.layerX);
+        image_anchor.set_y(e.layerY);
+        $("#pin_anchor").val(image_anchor.x + "," + image_anchor.y);
+        return false;
+    });
+    $("#the-image").mousemove(function(e) {
+        if(mousepressed){
+            image_anchor.set_x(e.layerX);
+            image_anchor.set_y(e.layerY);
+            $("#pin_anchor").val(image_anchor.x + "," + image_anchor.y);
+        }
+        return false;
+    });
+    $(document).mouseup(function(e) { mousepressed = false;});
+    $("#image-panel-background").mouseup(function(e) {$("#pin_anchor").focus();});
+});
