aa21548c1ddb0256a04184b7b72753120906da55
Fabio Montefuscolo <fabio.montefuscolo@gmail.com>
2011-07-18 11:48:42 -0300

    [fix] descomenta javascript que faz pins aparecer; [fix] 'workaround' que transforma latitude e longitude em float para serviço ajax

--- src/wp-content/themes/mapasdevista/js/front-end.js
+++ src/wp-content/themes/mapasdevista/js/front-end.js
@@ -1,18 +1,17 @@
 (function($){
 
-    
-    
+
+
     $(document).ready(function() {
-        
+
         mapstraction = new mxn.Mapstraction('map', mapinfo.api);
-        mapstraction.setImage(mapinfo.image_src);
-        
+
+        if(mapinfo.api === 'image')
+            mapstraction.setImage(mapinfo.image_src);
+
         mapstraction.applyFilter = function(o, f) {
             var vis = true;
-            
-            //console.log(o);
-            //console.log(f);
-            
+
             switch (f[1]) {
                 case 'ge':
                     if (o.getAttribute( f[0] ) < f[2]) {
@@ -25,14 +24,13 @@
                     }
                     break;
                 case 'eq':
-                    
+
                     if (o.getAttribute( f[0] ) != f[2]) {
-                        //console.log(o.getAttribute( f[0] ));
                         vis = false;
                     }
                     break;
                 case 'in':
-                    
+
                     if ( typeof(o.getAttribute( f[0] )) == 'undefined' ) {
                         vis = false;
                     } else if ( o.getAttribute( f[0] ).indexOf(f[2]) == -1 ) {
@@ -43,17 +41,17 @@
 
             return vis;
         };
-        
+
         mapstraction.setCenterAndZoom(new mxn.LatLonPoint(parseFloat(mapinfo.lat), parseFloat(mapinfo.lng)), parseInt(mapinfo.zoom));
-        
+
         if (mapinfo.api == 'googlev3') {
             mapstraction.setMapType(mxn.Mapstraction[mapinfo.type.toUpperCase()]);
         }
-        
-        
-        
+
+
+
         // Load posts
-        
+
         $.post(
             mapinfo.ajaxurl,
             {
@@ -67,11 +65,11 @@
                 loadPosts(totalPosts, 0);
             }
         );
-        
+
         function loadPosts(total, offset) {
-        
+
             var posts_per_page = 2;
-            
+
             $.ajax({
                 type: 'post',
                 url: mapinfo.ajaxurl,
@@ -86,100 +84,101 @@
                     posts_per_page: posts_per_page
                 },
                 success: function(data) {
-                    
+
                     console.log('loaded posts:'+offset);
-                    //console.log(data.posts);
-                    
+
                     if (data.newoffset != 'end') {
                         loadPosts(total, data.newoffset);
                     } else {
                         console.log('fim');
                     }
-                    
+
                     for (var p = 0; p < data.posts.length; p++) {
                         var pin = data.posts[p].pin;
                         var pin_size = [pin['1'], pin['2']];
-                        
-                        //var pin_anchor = [parseInt(pin['anchor']['x']), parseInt(pin['anchor']['y'])];
 
                         var ll = new mxn.LatLonPoint( data.posts[p].location.lat, data.posts[p].location.lon );
                         var marker = new mxn.Marker(ll);
 
-                        //marker.setIcon(pin[0], pin_size, pin_anchor);
-                        marker.setIcon(pin[0]);
+                        if(mapinfo.api !== 'image' && pin['anchor']) {
+                            var pin_anchor = [parseInt(pin['anchor']['x']), parseInt(pin['anchor']['y'])];
+                            marker.setIcon(pin[0], pin_size, pin_anchor);
+                        } else {
+                            marker.setIcon(pin[0]);
+                        }
+
                         marker.setAttribute( 'date', data.posts[p].date );
                         marker.setAttribute( 'post_type', data.posts[p].post_type );
                         marker.setAttribute( 'number', data.posts[p].number );
-                        
+
                         for (var att = 0; att < data.posts[p].terms.length; att++) {
-                        
+
                             if (typeof(marker.attributes[ data.posts[p].terms[att].taxonomy ]) != 'undefined') {
                                 marker.attributes[ data.posts[p].terms[att].taxonomy ].push(data.posts[p].terms[att].slug);
                             } else {
                                 marker.attributes[ data.posts[p].terms[att].taxonomy ] = [ data.posts[p].terms[att].slug ];
                             }
-                        
+
                         }
-                        
                         mapstraction.addMarker( marker );
-                    
+
                     }
-                    
+
                 }
-                
+
             });
-            
+
         }
-        
+
         // Filters events
-        
+
         $('.taxonomy-filter-checkbox').click(function() {
-        
+
             var tax = $(this).attr('name').replace('filter_by_', '').replace('[]', '');
             var val = $(this).val();
-            
+
             if ( $(this).attr('checked') ) {
                 mapstraction.addFilter(tax, 'in', val);
             } else {
                 mapstraction.removeFilter(tax, 'in', val);
             }
-            
+
             mapstraction.doFilter();
-        
+
         });
-        
+
         $('.post_type-filter-checkbox').click(function() {
-        
+
             var val = $(this).val();
-            
+
             if ( $(this).attr('checked') ) {
                 mapstraction.addFilter('post_type', 'eq', val);
             } else {
                 mapstraction.removeFilter('post_type', 'eq', val);
             }
-            
+
             mapstraction.doFilter();
-        
+
         });
-        
+
         $('#filter_by_new').click(function() {
-        
+
             if ( $(this).attr('checked') ) {
                 mapstraction.addFilter('number', 'le', 2);
             } else {
                 mapstraction.removeFilter('number', 'le', 2);
             }
-            
+
             mapstraction.doFilter();
-        
+
         });
-        
-        
-        
-        
-        
-    
+
+
+
+
+
+
     });
-    
+
 })(jQuery);
 
--- src/wp-content/themes/mapasdevista/mapasdevista-get-posts.php
+++ src/wp-content/themes/mapasdevista/mapasdevista-get-posts.php
@@ -8,38 +8,38 @@ add_action('wp_ajax_mapasdevista_get_posts', 'mapasdevista_get_posts');
 function mapasdevista_get_posts() {
 
     $mapinfo = get_post_meta($_POST['page_id'], '_mapasdevista', true);
-    
+
     if (!is_array($mapinfo['post_types']))
-        return; // nothing to show 
-    
+        return; // nothing to show
+
     if ($_POST['get'] == 'totalPosts') {
-    
-    
+
+
         global $wpdb;
-        
-        
+
+
         foreach ($mapinfo['post_types'] as $i => $p) {
             $mapinfo['post_types'][$i] = "'$p'";
         }
-        
+
         $pt = implode(',', $mapinfo['post_types']);
-        
+
         if ($_POST['api'] == 'image') {
             $q = "SELECT COUNT(DISTINCT(post_id)) FROM $wpdb->postmeta JOIN $wpdb->posts ON $wpdb->postmeta.post_id = $wpdb->posts.ID WHERE post_type IN ($pt) AND post_status = 'publish' AND meta_key = '_mpv_in_img_map' AND meta_value = '{$_POST['page_id']}'";
         } else {
             $q = "SELECT COUNT(post_id) FROM $wpdb->postmeta JOIN $wpdb->posts ON $wpdb->postmeta.post_id = $wpdb->posts.ID WHERE post_type IN ($pt) AND post_status = 'publish' AND meta_key = '_mpv_location'";
         }
-        
-        
+
+
         $total = $wpdb->get_var($q);
-        
+
         echo $total;
-    
-    
+
+
     } elseif ($_POST['get'] == 'posts') {
-    
+
         if ($_POST['api'] == 'image') {
-            
+
             $args = array(
                 'numberposts'     => $_POST['posts_per_page'],
                 'offset'          => $_POST['offset'],
@@ -48,8 +48,8 @@ function mapasdevista_get_posts() {
                 'meta_key'        => '_mpv_in_img_map',
                 'meta_value'      => $_POST['page_id'],
                 'post_type'       => $mapinfo['post_types'],
-            ); 
-        
+            );
+
         } else {
             $args = array(
                 'numberposts'     => $_POST['posts_per_page'],
@@ -60,44 +60,49 @@ function mapasdevista_get_posts() {
                 'post_type'       => $mapinfo['post_types'],
             );
         }
-        
+
         $posts = get_posts($args);
-        
+
         $postsResponse = array();
-        
+
         $number = $_POST['offset'];
-        
+
         foreach ($posts as $post) {
-            
-            
+
+
             if ($_POST['api'] == 'image') {
-            
+
                 $meta = get_post_meta($post->ID, '_mpv_img_coord_' . $_POST['page_id'], true);
                 $meta = explode(',', $meta);
-                
+
                 $location = array();
-                $location['lon'] = $meta[0];
-                $location['lat'] = $meta[1];
-                
+                $location['lon'] = floatval($meta[0]);
+                $location['lat'] = floatval($meta[1]);
+
                 $pin_id = get_post_meta($post->ID, '_mpv_img_pin_' . $_POST['page_id'], true);
                 $pin = wp_get_attachment_image_src($pin_id);
-                
 
-            
+
+
             } else {
-            
+
                 $location = get_post_meta($post->ID, '_mpv_location', true);
+
+                // wordpress doesn't serialize data correctly and openlayers
+                // only accept float values for latitude and longitude
+                if (isset($location['lat']) && isset($location['lon'])) {
+                    $location['lat'] = floatval($location['lat']);
+                    $location['lon'] = floatval($location['lon']);
+                }
+
                 $pin_id = get_post_meta($post->ID, '_mpv_pin', true);
                 $pin = wp_get_attachment_image_src($pin_id);
                 $pin['anchor'] = get_post_meta($pin_id, '_pin_anchor', true);
-                
-                
-            
             }
-            
+
             $number ++;
             $terms = wp_get_object_terms( $post->ID, $mapinfo['taxonomies'] );
-            
+
             $postsResponse[] = array(
                 'ID' => $post->ID,
                 'title' => $post->post_title,
@@ -108,26 +113,26 @@ function mapasdevista_get_posts() {
                 'number' => $number,
                 'pin' => $pin
             );
-            
-            
-            
-            
+
+
+
+
         }
-        
+
         $newoffset = (int) $_POST['offset'] + sizeof($posts) < (int) $_POST['total'] ? (int) $_POST['offset'] + (int) $_POST['posts_per_page'] : 'end';
-        
+
         $response = array(
-        
+
             'newoffset' => $newoffset,
             'posts' => $postsResponse
-        
+
         );
-        
+
         echo json_encode($response);
-    
-    
+
+
     }
-    
+
     die();
 
 }
