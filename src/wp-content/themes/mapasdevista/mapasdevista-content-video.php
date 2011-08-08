715ee0d54df12b387b32c2116fedd187fd9e902b
Leo Germani <leogermani@hacklab.com.br>
2011-08-08 12:01:39 -0300

    post format loop

--- src/wp-content/themes/mapasdevista/mapasdevista-content-video.php
+++ src/wp-content/themes/mapasdevista/mapasdevista-content-video.php
@@ -1,1 +1,22 @@
-<h1>VIDEO</h1>
+<?php
+
+$content = get_the_content();
+
+
+// gets the first video in the post
+preg_match_all('|http://[^"\'\s]+|', $content, $m);
+foreach ($m[0] as $match) {
+    if ( preg_match('#http://(www\.)?youtube.com/watch.*#i', $match) ) {
+        $video = $match;
+        break;
+    }
+    if ( preg_match('#http://(www\.)?vimeo\.com/.*#i', $match) ) {
+        $video = $match;
+        break;
+    }
+}
+
+?>
+
+
+<?php echo apply_filters('the_content', $video); ?>
