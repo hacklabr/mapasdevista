80293a006350733865e2f956029f36f0b47135a5
Anderson Orui <contato@andersonorui.com.br>
2011-08-18 14:44:46 -0300

    adicionando lista de categoria e tags e botao de editar post

--- src/wp-content/themes/mapasdevista/mapasdevista-content.php
+++ src/wp-content/themes/mapasdevista/mapasdevista-content.php
@@ -1,8 +1,23 @@
 <section id="entry-content" class="clearfix">
 
     <?php the_content(); ?>
-    <?php wp_link_pages( array( 'before' => '<div class="page-link"><span>' . __( 'Pages:', 'twentyeleven' ) . '</span>', 'after' => '</div>' ) ); ?>
-    
-    <?php comments_template(); ?>
 
 </section>
+
+<footer class="entry-meta">
+    <?php
+        $categories_list = get_the_category_list( __( ', ', 'mapasdevista' ) );
+        $tag_list = get_the_tag_list( '', __( ', ', 'mapasdevista' ) );
+    ?>
+
+    <?php if($categories_list) : ?>
+        <p class="bottom"><?php _e("Categories: ", "mapasdevista"); echo $categories_list; ?></p>
+    <?php endif; ?>
+
+    <?php if($tag_list) : ?>
+        <p><?php _e("Tags: ", "mapasdevista"); echo $tag_list; ?></p>
+    <?php endif; ?>
+
+</footer>
+
+<?php comments_template(); ?>
