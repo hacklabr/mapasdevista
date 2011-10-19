e1e2701f78abc2c856d82d905b67c069d1c19f8e
Rafael Freitas <rafael@hacklab.com.br>
2011-10-19 19:22:18 -0200

    [fix] corrigido bug ( IE <= 8 ) no começo do arquivo front-end.js       removido o rgba nos IE <= 8       substituido o article por div

--- src/wp-content/themes/mapasdevista/mapasdevista-loop-bubble.php
+++ src/wp-content/themes/mapasdevista/mapasdevista-loop-bubble.php
@@ -6,7 +6,7 @@
             <p class="metadata bottom">
                 <span class="date"><?php the_time( get_option('date_format') ); ?></span>
             </p>
-            <h1 class="bottom"><a class="js-link-to-post" id="balloon-post-link-<?php the_ID(); ?>" href="<?php the_permalink(); ?>" onClick="return mapasdevista.linkToPost(this);"><?php the_title(); ?></a></h1>
+            <h1 class="bottom"><a class="js-link-to-post" id="balloon-post-link-<?php the_ID(); ?>" href="<?php the_permalink(); ?>" onClick="mapasdevista.linkToPost(this); return false;"><?php the_title(); ?></a></h1>
             <?php mapasdevista_get_template( 'mapasdevista-bubble', get_post_format() ); ?>
         </div>
     </div>
