86e9110cb4b1b35a88bceb43046b24c162d1a89e
Leo Germani <leogermani@hacklab.com.br>
2012-08-13 08:56:19 -0300

    exploring leaflet LayerGroups

--- js/front-end.js
+++ js/front-end.js
@@ -66,6 +66,8 @@
                         jQuery('#posts-loader').hide();
                     }
                 
+                    Taxonomies = {};
+                    category = new L.LayerGroup(marker);;
                     
                     for (var p = 0; p < data.posts.length; p++) {
                         
@@ -105,22 +107,38 @@
                             //marker.setHover = true;
                             //marker.click.addHandler(function(event) { console.log(event); });
                             
-                            /*
+                            
+                            
                             for (var att = 0; att < data.posts[p].terms.length; att++) {
-
-                                if (typeof(marker.attributes[ data.posts[p].terms[att].taxonomy ]) != 'undefined') {
-                                    marker.attributes[ data.posts[p].terms[att].taxonomy ].push(data.posts[p].terms[att].slug);
+                                
+                                if (data.posts[p].terms[att].taxonomy == 'category')
+                                    category.addLayer(marker);
+                                
+                                if (typeof(Taxonomies[ data.posts[p].terms[att].taxonomy ]) != 'undefined') {
+                                    //marker.attributes[ data.posts[p].terms[att].taxonomy ].push(data.posts[p].terms[att].slug);
+                                    if (typeof(Taxonomies[ data.posts[p].terms[att].taxonomy ][ data.posts[p].terms[att].slug ]) != 'undefined')
+                                        Taxonomies[ data.posts[p].terms[att].taxonomy ][ data.posts[p].terms[att].slug ].addLayer(marker);
+                                    else
+                                        Taxonomies[ data.posts[p].terms[att].taxonomy ][ data.posts[p].terms[att].slug ] = new L.LayerGroup(marker);
+                                    
                                 } else {
-                                    marker.attributes[ data.posts[p].terms[att].taxonomy ] = [ data.posts[p].terms[att].slug ];
+                                    Taxonomies[ data.posts[p].terms[att].taxonomy ] = {};
+                                    Taxonomies[ data.posts[p].terms[att].taxonomy ][ data.posts[p].terms[att].slug ] = new L.LayerGroup(marker);
+                                    //marker.attributes[ data.posts[p].terms[att].taxonomy ] = [ data.posts[p].terms[att].slug ];
                                 }
+                                
+                                console.log(Taxonomies);
 
                             }
-                            * */
+                            
                         }
                         
                         $('#balloon_' + data.posts[p].ID).remove();
-
-                        leaflet.addLayer(marker);
+                        
+                        //category.addTo('map');
+                        //leaflet.addLayer(Taxonomies);
+                        //leaflet.addLayer(marker);
+                        
                         
                         /*
                         if (mapinfo.api == 'openlayers' && pin['clickable']) {
@@ -131,12 +149,140 @@
                         */ 
 
                     }
-
+                    
+                    
+                    console.log(Taxonomies);
+                    console.log(Taxonomies.length);
+                    
+                    for (var tax in Taxonomies) {
+                        for (var term in Taxonomies[tax]) {
+                            leaflet.addLayer(Taxonomies[tax][term]);
+                        }
+                    }
+                    
                 }
 
             });
+            
 
         }
+        
+        // Filters events
+          /*  
+        $("#logical_oparator input").click(function (){
+            mapstraction.logicalOperator = $(this).val();
+            mapstraction.doFilter();
+        });
+
+        mapstraction.logicalOperator = $("#logical_oparator input").val();
+        */
+        
+        $('.taxonomy-filter-checkbox').click(function() {
+
+            var tax = $(this).attr('name').replace('filter_by_', '').replace('[]', '');
+            var term = $(this).val();
+
+            if ( $(this).attr('checked') ) {
+                leaflet.addLayer(Taxonomies[tax][term]);
+            } else {
+                leaflet.removeLayer(Taxonomies[tax][term]);
+            }
+
+            mapstraction.doFilter();
+            updateResults();
+
+        });
+
+        $('.post_type-filter-checkbox').click(function() {
+
+            var val = $(this).val();
+
+            if ( $(this).attr('checked') ) {
+                mapstraction.addFilter('post_type', 'eq', val);
+            } else {
+                mapstraction.removeFilter('post_type', 'eq', val);
+            }
+
+            mapstraction.doFilter();
+            updateResults();
+
+        });
+        
+        $('.author-filter-checkbox').click(function() {
+
+            var val = $(this).val();
+
+            if ( $(this).attr('checked') ) {
+                mapstraction.addFilter('author', 'eq', val);
+            } else {
+                mapstraction.removeFilter('author', 'eq', val);
+            }
+
+            mapstraction.doFilter();
+            updateResults();
+
+        });
+        
+        hWindow = $(window).height();
+        
+        $("#toggle-filters").toggle(
+            function() {
+                $(this).html("<img src='"+mapinfo.baseurl+"/img/hide-filters.png'/> " + messages.hide_filters);
+                
+                $('.hide_when_show_filters').hide();
+                
+                $(this).parent().animate({
+                    "bottom": hWindow/3
+                }, 450);
+                $("#filters").animate({
+                    height: (hWindow/3)
+                }, 450);
+                
+            },
+            function() {
+                $(this).html("<img src='"+mapinfo.baseurl+"/img/show-filters.png'/> " + messages.show_filters);
+                
+                $('.show_when_hide_filters').hide();
+                
+                $(this).parent().animate({
+                    "bottom": "0"
+                }, 450);
+                $("#filters").animate({
+                    height: "0"
+                }, 450);
+            }
+            );
+        
+        $("#toggle-side-menu").hover(
+            function() {
+                $(".map-menu-side").show();
+            },
+            function() {
+                $(".map-menu-side").hide();
+            }
+            );
+
+        $(".map-menu-side").hover(
+            function() {
+                $(this).show();
+            },
+            function() {
+                $(this).hide();
+            }
+            );
+        
+        $("#toggle-results").toggle(
+            function() { 
+                $(this).find("img").attr("src",mapinfo.baseurl+"/img/hide-results.png");
+                $('.hide_when_show_results').hide();
+                $("#results").show();
+            },
+            function() { 
+                $(this).find("img").attr("src",mapinfo.baseurl+"/img/show-results.png");
+                $('.show_when_hide_results').hide();
+                $("#results").hide();
+            }
+            );
         
         
     });
